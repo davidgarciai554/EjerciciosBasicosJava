481a703d09d7254a0a3e2182455977e2292e07e1
usuario <usuario@David>
2019-11-28 16:58:14 +0100

    El segundo ejercicio empezado

--- src/ejerciciosbasicojava/EjerciciosBasicoJava.java
+++ src/ejerciciosbasicojava/EjerciciosBasicoJava.java
@@ -15,11 +15,15 @@ public class EjerciciosBasicoJava {
             return true;
         }
         return false;  
+    } 
+    public int multa(int velocidad,boolean cumpleanos){
+        return 0; 
     }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //1ºEjercicio
         EjerciciosBasicoJava ejercicio =new EjerciciosBasicoJava();
         System.out.println(ejercicio.fiestaArdillas(30,false));
         System.out.println(ejercicio.fiestaArdillas(50,false));
