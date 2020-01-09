f59fc962cd0d007c9f44cbebbd4298e8e4cb4454
usuario <usuario@David>
2020-01-09 12:13:36 +0100

    Ejercicios basicos terminados

--- src/ejerciciosbasicojava/EjerciciosBasicoJava.java
+++ src/ejerciciosbasicojava/EjerciciosBasicoJava.java
@@ -47,10 +47,54 @@ public class EjerciciosBasicoJava {
         return false;
     }
     public boolean menorPor10(int a,int b,int c){
-        if(a>10||b>10||c>10){
+        if(Math.abs(a-b)>=10||Math.abs(a-c)>=10||Math.abs(c-b)>=10){
             return true;
         }
         return false;
+    }
+    public boolean digitoIgual(int a,int b){
+        if(a/10==b/10||a/10==b%10||a%10==b%10||a%10==b/10){
+            return true;
+        }
+        return false;
+    }
+    public boolean multiplo35(int a){
+        if((a%3==0 && a%5==0)){
+            return false;
+        }
+        if((a%3==0 || a%5==0)){
+            return true;
+        }
+        
+        return false;
+    }
+    public boolean menor20(int a){
+        while(a>20){
+            a-=20;
+        }
+        if(a+2==20||a+1==20){
+            return true;
+        }
+        return false;
+    }
+    public int loteria(int a,int b,int c){
+        if (a==b&&b==c&&a==2){
+            return 10;
+        }
+        if (a==b&&b==c){
+            return 5;
+        }
+        if(b==c){
+            return 1;
+        }
+        return 0;
+    }
+    public int da2(int a,int b,boolean dobles){
+        int aux=a+b;
+        if(dobles&&a==b){
+            aux++;
+        }
+        return aux;
     }
     /**
      * @param args the command line arguments
@@ -78,9 +122,27 @@ public class EjerciciosBasicoJava {
         System.out.println(ejercicio.menorPor10(5,7,11));
         System.out.println(ejercicio.menorPor10(11,7,1));
         System.out.println(ejercicio.menorPor10(5,7,10));
-        
-        
-        
+        //6ºEjercicio
+        System.out.println(ejercicio.digitoIgual(12,23));
+        System.out.println(ejercicio.digitoIgual(10,47));
+        System.out.println(ejercicio.digitoIgual(22,72));
+        //7ºEjercicio
+        System.out.println(ejercicio.multiplo35(3));
+        System.out.println(ejercicio.multiplo35(5));
+        System.out.println(ejercicio.multiplo35(15));
+        //8ºEjercicio
+        System.out.println(ejercicio.menor20(18));
+        System.out.println(ejercicio.menor20(59));
+        System.out.println(ejercicio.menor20(80));
+        //9ºEjercicio
+        System.out.println(ejercicio.loteria(2,2,2));
+        System.out.println(ejercicio.loteria(1,2,2));
+        System.out.println(ejercicio.loteria(0,0,0));
+        //10ºEjercicio
+        System.out.println(ejercicio.da2(2,3,true));
+        System.out.println(ejercicio.da2(3,3,false));
+        System.out.println(ejercicio.da2(3,3,true));
+        System.out.println(ejercicio.da2(6,6,true));
         
     }
     
